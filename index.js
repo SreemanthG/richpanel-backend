8c5d27f2d5e6ff7f20762a4f9e6db08fbd1ac4b6
sreemanthg <sreemanth2001@gmail.com>
2021-08-10 21:35:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,94 @@
+require('dotenv').config();
+
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  { urlencoded, json } = require('body-parser'),
+  app = express();
+
+// Parse application/x-www-form-urlencoded
+app.use(urlencoded({ extended: true }));
+
+// Parse application/json
+app.use(json());
+
+// Respond with 'Hello World' when a GET request is made to the homepage
+app.get('/', function (_req, res) {
+  res.send('Hello World');
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
+// Creates the endpoint for your webhook
+app.post('/webhook', (req, res) => {
+  let body = req.body;
+
+  // Checks if this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the body of the webhook event
+      let webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+
+      // Get the sender PSID
+      let senderPsid = webhookEvent.sender.id;
+      console.log('Sender PSID: ' + senderPsid);
+
+      if (webhookEvent.message) {
+        handleMessage(senderPsid, webhookEvent.message);
+      } 
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+});
+
+// Handles messages events
+function handleMessage(senderPsid, receivedMessage) {
+  let response='';
+
+  // Checks if the message contains text
+  if (receivedMessage.text) {
+    console.log(recievedMessage.text);
+  } 
+
+}
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
